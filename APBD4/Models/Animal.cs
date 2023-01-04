6c490951d92dc54d129d124cc442ba27f3555e67
Marta Peska <s22323@pjwstk.edu.pl>
2023-01-04 18:26:13 +0100

    dodano walidację

--- APBD4/Models/Animal.cs
+++ APBD4/Models/Animal.cs
@@ -1,12 +1,19 @@
-﻿namespace APBD4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD4.Models
 {
     public class Animal
     {
+        [Required(ErrorMessage = "Name cant be empty")]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [MaxLength(200)]
         public string Description { get; set; }
-
+        [Required(ErrorMessage = "Category cant be empty")]
+        [MaxLength(100)]
         public string Category { get; set; }
-
+        [Required(ErrorMessage = "Area cant be empty")]
+        [MaxLength(100)]
         public string Area { get; set; }
     }
 }
